48670ef321bf25a439c5aaf95487c43f2ad75ede
Kim-JW <bbam03@naver.com>
2022-01-17 14:24:34 +0900

    jstlel practice #01

--- jstlel/src/main/java/jstlel/_01Servlet.java
+++ jstlel/src/main/java/jstlel/_01Servlet.java
@@ -1,26 +1,20 @@
-package main.java.helloweb;
+package jstlel;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet {
+public class _01Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 요청처리(request handle)
-		// 응답 
 		
-		response.setContentType("text/html; charset=utf-8");
 		
-		PrintWriter pw = response.getWriter();
-		pw.print("<h1> Hello </h1>");
-		pw.print("<h1>안녕하세요</h1>");
+		request
+			.getRequestDispatcher("/WEB-INF/views/01.jsp")
+			.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
